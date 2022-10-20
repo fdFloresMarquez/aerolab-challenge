0bd113c3196743639ad9b91d6f475bd3310f17a1
Facundo <floresfacundo68@gmail.com>
2022-10-20 15:59:30 -0300

    fix alias @ vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import * as path from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -7,11 +7,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(path.resolve(__dirname), 'src'),
-      },
-    ],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
