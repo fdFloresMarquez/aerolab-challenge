ee520c593f8ead5448a980f47dad187e3127b88e
Facundo <floresfacundo68@gmail.com>
2022-10-26 12:20:31 -0300

    add loading feature to CoinButton Component

--- src/app/layout/CoinButton.tsx
+++ src/app/layout/CoinButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image, Stack, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Center, CircularProgress, Image, Stack, Text } from '@chakra-ui/react';
 
 import coin from '@/assets/icons/coin.svg';
 import { usePoints } from '@/user/hooks';
@@ -10,6 +10,13 @@ interface Props {
 
 export const CoinButton: React.FC<Props> = ({ points }) => {
   const [userPoints, addPoints] = usePoints();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = async (): Promise<void> => {
+    setIsLoading(true);
+    await addPoints(points);
+    setIsLoading(false);
+  };
 
   return (
     <Stack
@@ -20,12 +27,19 @@ export const CoinButton: React.FC<Props> = ({ points }) => {
       direction="row"
       paddingX={3}
       paddingY={2}
-      onClick={() => addPoints(points)}
+      userSelect="none"
+      onClick={handleClick}
     >
-      <Text fontWeight="500" userSelect="none">
-        {points}
-      </Text>
-      <Image height={6} src={coin} width={6} />
+      {isLoading ? (
+        <Center>
+          <CircularProgress isIndeterminate color="primary.400" size="30px" />
+        </Center>
+      ) : (
+        <>
+          <Text fontWeight="500">{points}</Text>
+          <Image height={6} src={coin} width={6} />
+        </>
+      )}
     </Stack>
   );
 };
